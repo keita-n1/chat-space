961d00f4ae1b51a38be1bc4563f1fd21c7bdf7da
keita.nakagawa <saku.nk@icloud.com>
2019-05-11 15:07:17 +0900

    修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,10 +6,6 @@ class Group < ApplicationRecord
   validates :name, presence: true
 
   def show_last_message
-    if (last_message = messages.last).present?
-      last_message.content? ? last_message.content : '画像が投稿されています'
-    else
-      'まだメッセージがありません'
-    end
+    last_message.content && ‘画像が投稿されています’
   end
 end
