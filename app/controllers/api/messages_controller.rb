cc7f2e62f46652b6efc5eea4d83820a5e97a415c
keita.nakagawa <saku.nk@icloud.com>
2019-05-20 02:32:58 +0900

    Update messages_controller.rb

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,8 +1,10 @@
 class Api::MessagesController < ApplicationController
   def index
+    @group = Group.find(params[:group_id])
     @messages = Message.all 
     respond_to do |format|
       format.html
-      format.json { @new_message = Group.find(paramas[:id]).Message.where('id > ?', params[:id]) }
+      format.json { @new_message = @group.messages.where('id > ?', params[:id]) }
+    end
   end
 end
