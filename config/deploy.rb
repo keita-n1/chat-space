3250e0192e8c3a72e02cffe513af48bf3377566f
keita.nakagawa <saku.nk@icloud.com>
2019-05-23 15:12:53 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,13 @@
 # config valid only for current version of Capistrano
-lock '<3.11.0>'
+lock '3.11.0'
 
 set :application, 'chat-space'
-set :repo_url,  'git@github.com:<keita-n1>/<chat-space>.git'
+set :repo_url,  'git@github.com:keita-n1/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '<2.5.1>' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/you.pem']
