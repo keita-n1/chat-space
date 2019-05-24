662b16256588b733ad7cacc28a1c719546fede7e
keita.nakagawa <saku.nk@icloud.com>
2019-05-24 17:35:00 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'you-n1'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/you-n1'
+  config.fog_directory  = 'you2'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/you-2'
 end
